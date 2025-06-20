725b07f94d5164ac54dc73c8bb7fba6b184b1126
SHANKAR-YADAVA <shankaryadavar2004@gmail.com>
2025-06-20 11:28:43 +0530

    Add Tailwind CSS configuration with custom theme settings and utilities

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  {
+    ignores: ['dist', '.eslintrc.cjs'], // Add .eslintrc.cjs to ignore list
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -16,18 +19,27 @@ export default [
         sourceType: 'module',
       },
     },
+    settings: {
+      react: {
+        version: '18.2', // Explicit React version
+      },
+    },
     plugins: {
+      react, // plugin:react/recommended
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules, // plugin:react/recommended
+      ...react.configs['jsx-runtime'].rules, // plugin:react/jsx-runtime
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'react/prop-types': 0, // Add missing rule
     },
   },
 ]
